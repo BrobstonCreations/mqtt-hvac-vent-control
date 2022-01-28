39eb9a94c3357dc697d4b45f117a23d38fc954ff
TonyBrobston <anthonybrobston@live.com>
2022-01-27 19:35:00 -0600

    feat: handle more thermostat scenarios

--- src/services/controllerService.ts
+++ src/services/controllerService.ts
@@ -45,16 +45,23 @@ const actThermostat = async (house: State.House, client: AsyncMqttClient): Promi
     const thermostatHeatModePayload = mapMemoryToTopic['thermostat.heatModePayload'];
     const thermostatCoolModePayload = mapMemoryToTopic['thermostat.coolModePayload'];
     const thermostatTargetTemperatureCommandTopic = mapMemoryToTopic['thermostat.targetTemperatureCommandTopic'];
-    if (turnHvacOn(house, thermostatCoolModePayload, thermostatHeatModePayload)
-        && house.thermostat.actualTemperature
-        && house.thermostat.targetTemperature
-        && house.thermostat.actualTemperature === house.thermostat.targetTemperature) {
-        if (house.thermostat.mode === thermostatHeatModePayload) {
-            const targetTemperature = house.thermostat.actualTemperature + 1;
-            await client.publish(thermostatTargetTemperatureCommandTopic, targetTemperature.toString());
-        } else if (house.thermostat.mode === thermostatCoolModePayload) {
-            const targetTemperature = house.thermostat.actualTemperature - 1;
-            await client.publish(thermostatTargetTemperatureCommandTopic, targetTemperature.toString());
+    if (house.thermostat.actualTemperature && house.thermostat.targetTemperature) {
+        if (turnHvacOn(house, thermostatCoolModePayload, thermostatHeatModePayload)
+            && house.thermostat.targetTemperature
+            && house.thermostat.actualTemperature === house.thermostat.targetTemperature) {
+            if (house.thermostat.mode === thermostatHeatModePayload) {
+                const targetTemperature = house.thermostat.actualTemperature + 1;
+                await client.publish(thermostatTargetTemperatureCommandTopic, targetTemperature.toString());
+            } else if (house.thermostat.mode === thermostatCoolModePayload) {
+                const targetTemperature = house.thermostat.actualTemperature - 1;
+                await client.publish(thermostatTargetTemperatureCommandTopic, targetTemperature.toString());
+            }
+        } else if (turnHvacOn(house, thermostatCoolModePayload, thermostatHeatModePayload)) {
+            await client.publish(thermostatTargetTemperatureCommandTopic,
+                house.thermostat.targetTemperature.toString());
+        } else {
+            await client.publish(thermostatTargetTemperatureCommandTopic,
+                house.thermostat.actualTemperature.toString());
         }
     }
 };
