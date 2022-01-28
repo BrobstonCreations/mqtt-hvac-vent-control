2aefb09f7e744d15a54f18f846a71d9c20e08634
TonyBrobston <anthonybrobston@live.com>
2022-01-27 18:14:16 -0600

    test: add more scenarios to turnHvacOn

--- tests/services/roomService.test.ts
+++ tests/services/roomService.test.ts
@@ -9,13 +9,6 @@ describe('roomService', () => {
     const thermostatHeatModePayload = chance.word();
     it.each([
         {
-            actualRoomTemperature: null,
-            expected: false,
-            name: 'should not turn on hvac if all null',
-            targetRoomTemperature: null,
-            thermostatMode: null,
-        },
-        {
             actualRoomTemperature: 72,
             expected: true,
             name: 'should turn on hvac if thermostat is in heat mode and actual room temperature is less than target room temperature',
@@ -28,6 +21,27 @@ describe('roomService', () => {
             name: 'should turn on hvac if thermostat is in cool mode and actual room temperature is greater than target room temperature',
             targetRoomTemperature: 71,
             thermostatMode: thermostatCoolModePayload,
+        },
+        {
+            actualRoomTemperature: 72,
+            expected: false,
+            name: 'should not turn on hvac if thermostat is in heat mode and actual room temperature is equal to target room temperature',
+            targetRoomTemperature: 72,
+            thermostatMode: thermostatHeatModePayload,
+        },
+        {
+            actualRoomTemperature: 72,
+            expected: false,
+            name: 'should not turn on hvac if thermostat is in cool mode and actual room temperature is equal to target room temperature',
+            targetRoomTemperature: 72,
+            thermostatMode: thermostatCoolModePayload,
+        },
+        {
+            actualRoomTemperature: null,
+            expected: false,
+            name: 'should not turn on hvac if all null',
+            targetRoomTemperature: null,
+            thermostatMode: null,
         },
     ])('$name', ({
         actualRoomTemperature,
