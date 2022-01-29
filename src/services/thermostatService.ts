5be18161e940085d393e3371343b0fd36e80690d
TonyBrobston <anthonybrobston@live.com>
2022-01-28 21:39:24 -0600

    fix: only begin heating or cooling if idle

--- src/services/thermostatService.ts
+++ src/services/thermostatService.ts
@@ -8,20 +8,35 @@ export const actThermostat = async (house: State.House, client: AsyncMqttClient)
     const thermostatHeatModePayload = mapMemoryToTopic['thermostat.heatModePayload'];
     const thermostatCoolModePayload = mapMemoryToTopic['thermostat.coolModePayload'];
     const thermostatTargetTemperatureCommandTopic = mapMemoryToTopic['thermostat.targetTemperatureCommandTopic'];
-    if (house.thermostat.actualTemperature && house.thermostat.targetTemperature) {
+    if (house.thermostat.actualTemperature) {
         if (atLeastOneRoomNeedsHeatedOrCooled(house, thermostatCoolModePayload, thermostatHeatModePayload)) {
-            if (house.thermostat.mode === thermostatHeatModePayload) {
-                const targetTemperature = house.thermostat.actualTemperature + 1;
-                await client.publish(thermostatTargetTemperatureCommandTopic, targetTemperature.toString());
-            } else if (house.thermostat.mode === thermostatCoolModePayload) {
-                const targetTemperature = house.thermostat.actualTemperature - 1;
+            if (house.thermostat.action === mapMemoryToTopic['thermostat.idleActionPayload']) {
+                const difference = determineDifference(
+                    house.thermostat,
+                    thermostatHeatModePayload,
+                    thermostatCoolModePayload,
+                );
+                const targetTemperature = house.thermostat.actualTemperature + difference;
                 await client.publish(thermostatTargetTemperatureCommandTopic, targetTemperature.toString());
             }
-        } else if (atLeastOneRoomNeedsHeatedOrCooled(house, thermostatCoolModePayload, thermostatHeatModePayload)) {
-            return;
         } else {
             await client.publish(thermostatTargetTemperatureCommandTopic,
                 house.thermostat.actualTemperature.toString());
         }
+    }
+};
+
+const determineDifference = (
+    thermostat: State.Thermostat,
+    thermostatHeatModePayload: string,
+    thermostatCoolModePayload: string,
+): number => {
+    switch (thermostat.mode) {
+        case thermostatHeatModePayload:
+            return 1;
+        case thermostatCoolModePayload:
+            return -1;
+        default:
+            return 0;
     }
 };
