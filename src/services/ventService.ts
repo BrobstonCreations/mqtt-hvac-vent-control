02c1e62ea961688f82fb3c84a61f3b5c29e99740
TonyBrobston <anthonybrobston@live.com>
2022-07-05 12:21:13 -0500

    refactor: remove asyc/await from foreach

--- src/services/ventService.ts
+++ src/services/ventService.ts
@@ -1,11 +1,13 @@
 import {AsyncMqttClient} from 'async-mqtt';
 import {House, Room, Vent} from '../types/Mqtt';
 
-export const adjustVents = async ({rooms, thermostat}: House, messages: {[key: string]: string|number}, client: AsyncMqttClient): Promise<void> => {
+export const adjustVents =
+    async (house: House, messages: {[key: string]: string|number}, client: AsyncMqttClient): Promise<void> => {
+    const {rooms, thermostat}: House = house;
     const thermostatCoolModePayload = thermostat.coolModePayload;
     const thermostatHeatModePayload = thermostat.heatModePayload;
     rooms.forEach((room: Room) => {
-        room.vents.forEach(async (vent: Vent) => {
+        room.vents.forEach((vent: Vent) => {
             const actualTemperature = messages[room.actualTemperatureStateTopic];
             const targetTemperature = messages[room.targetTemperatureStateTopic];
             if (actualTemperature && targetTemperature) {
@@ -17,7 +19,7 @@ export const adjustVents = async ({rooms, thermostat}: House, messages: {[key: s
                     if (!ventPosition || ventPosition && ventPosition !== ventStatePayload) {
                         const ventPositionPayload = actualTemperature < targetTemperature ?
                             vent.openPositionPayload : vent.closePositionPayload;
-                        await client.publish(vent.positionCommandTopic, ventPositionPayload);
+                        client.publish(vent.positionCommandTopic, ventPositionPayload);
                     }
                 } else if (thermostatMode === thermostatCoolModePayload) {
                     const ventStatePayload = actualTemperature < targetTemperature ?
@@ -25,7 +27,7 @@ export const adjustVents = async ({rooms, thermostat}: House, messages: {[key: s
                     if (!ventPosition || ventPosition && ventPosition !== ventStatePayload) {
                         const ventPositionPayload = actualTemperature <= targetTemperature ?
                             vent.closePositionPayload : vent.openPositionPayload;
-                        await client.publish(vent.positionCommandTopic, ventPositionPayload);
+                        client.publish(vent.positionCommandTopic, ventPositionPayload);
                     }
                 }
             }
