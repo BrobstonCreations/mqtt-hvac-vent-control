0237db817216410a3db9cc2a1503de8a341b9258
TonyBrobston <anthonybrobston@live.com>
2022-07-09 11:43:54 -0500

    refactor: refactor vent service

--- src/services/ventService.ts
+++ src/services/ventService.ts
@@ -1,19 +1,19 @@
 import {AsyncMqttClient} from 'async-mqtt';
 import {House, Room, Vent} from '../types/Mqtt';
 
-export const adjustVents =
-    async (house: House, messages: {[key: string]: string|number}, client: AsyncMqttClient): Promise<void> => {
-    const {rooms, thermostat}: House = house;
-    const thermostatCoolModePayload = thermostat.coolModePayload;
-    const thermostatHeatModePayload = thermostat.heatModePayload;
-    await Promise.all(rooms.map((room: Room) =>
+export const adjustVents = async (
+    house: House,
+    messages: {[key: string]: string|number},
+    client: AsyncMqttClient,
+): Promise<void> => {
+    await Promise.all(house.rooms.map((room: Room) =>
         room.vents.map((vent: Vent) => {
             const roomActualTemperature = messages[room.actualTemperatureStateTopic];
             const roomTargetTemperature = messages[room.targetTemperatureStateTopic];
             if (roomActualTemperature && roomTargetTemperature) {
-                const thermostatMode = messages[thermostat.modeStateTopic];
+                const thermostatMode = messages[house.thermostat.modeStateTopic];
                 const ventPosition = messages[vent.positionStateTopic];
-                if (thermostatMode === thermostatHeatModePayload) {
+                if (thermostatMode === house.thermostat.heatModePayload) {
                     const ventStatePayload = roomActualTemperature < roomTargetTemperature ?
                         vent.openedStatePayload : vent.closedStatePayload;
                     if (!ventPosition || ventPosition && ventPosition !== ventStatePayload) {
@@ -21,7 +21,7 @@ export const adjustVents =
                             vent.openPositionPayload : vent.closePositionPayload;
                         return client.publish(vent.positionCommandTopic, ventPositionPayload);
                     }
-                } else if (thermostatMode === thermostatCoolModePayload) {
+                } else if (thermostatMode === house.thermostat.coolModePayload) {
                     const ventStatePayload = roomActualTemperature <= roomTargetTemperature ?
                         vent.closedStatePayload : vent.openedStatePayload;
                     if (!ventPosition || ventPosition && ventPosition !== ventStatePayload) {
